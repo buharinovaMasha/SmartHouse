089fdfa2a9ad094dd17f874a604684f0f72e6e9a
Мария Бухаринова <buharinova.masha@yandex.ru>
2024-12-19 22:56:16 +0500

    Добавление регистрации роутеров

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.core.routes import register_routers
 
-
-@app.get("/ping")
-async def ping():
-    return {"ping": "pong!"}
+register_routers(app)
